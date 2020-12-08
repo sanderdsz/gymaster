fa3f2181cb6acc4d2bf1f6eec547524953740de0
sanderdsz <sanderdsz@gmail.com>
2020-12-07 22:13:32 -0300

    project structure ready and css start

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const routes = require('./routes')
 const server = express()
 
+server.use(express.static('public'))
+server.use(routes)
 server.set("view engine", "html")
 
 nunjucks.configure("views", {
-    express:server
-})
-
-server.get('/', function(req, res) {
-    return res.render("index")
+    express:server,
+    autoescape: false,
+    noCache: true
 })
 
 server.listen(5000, function() {
-    console.log("Hello world, server is running.")
+    console.log("The server is running...")
 }) 
