1b5d183429f4b74d99cb634ceac48bb1d6733ebb
sanderdsz <sanderdsz@gmail.com>
2020-12-09 09:35:14 -0300

    instructors creation page added

--- routes.js
+++ routes.js
@@ -9,6 +9,14 @@ routes.get('/instructors', function(req, res) {
     return res.render('instructors/index')
 })
 
+routes.get('/instructors/create', function(req, res){
+    return res.render('instructors/create/index')
+})
+
+routes.post('/instructors', function(req, res){
+    return res.send('ok')
+})
+
 routes.get('/members', function(req, res) {
     return res.send('members')
 })
