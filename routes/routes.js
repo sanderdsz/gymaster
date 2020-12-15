c9b551cd0ae9ee4eb308aa35651fe3fa7f8217e6
sanderdsz <sanderdsz@gmail.com>
2020-12-15 07:53:19 -0300

    fields component added

--- routes/routes.js
+++ routes/routes.js
@@ -6,17 +6,16 @@ const instructors = require('./instructors')
 routes.get('/', function(req, res) {
     return res.redirect('/instructors')
 })
-
 routes.get('/instructors', function(req, res) {
     return res.render('instructors/index')
 })
-
 routes.get('/instructors/create', function(req, res){
     return res.render('instructors/create/index')
 })
-
 routes.get('/instructors/:id', instructors.show)
-
+routes.get('/instructors/:id/edit',function(req, res){
+    return res.render('instructors/edit/index')
+})
 routes.post('/instructors', instructors.post) 
 
 routes.get('/members', function(req, res) {
