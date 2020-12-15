5fbcf77163aa13259b6d066acb98752369b5430a
sanderdsz <sanderdsz@gmail.com>
2020-12-15 17:48:18 -0300

    delete function added

--- routes/routes.js
+++ routes/routes.js
@@ -3,21 +3,15 @@ const routes = express.Router()
 
 const instructors = require('./instructors')
 
-routes.get('/', function(req, res) {
-    return res.redirect('/instructors')
-})
-routes.get('/instructors', function(req, res) {
-    return res.render('instructors/index')
-})
-routes.get('/instructors/create', function(req, res){
-    return res.render('instructors/create/index')
-})
+routes.get('/', function(req, res) { return res.redirect('/instructors') })
+routes.get('/instructors', function(req, res) { return res.render('instructors/index') })
+routes.get('/instructors/create', function(req, res){ return res.render('instructors/create/index') })
 routes.get('/instructors/:id', instructors.show)
 routes.get('/instructors/:id/edit', instructors.edit)
-routes.post('/instructors', instructors.post) 
+routes.post('/instructors', instructors.post)
+routes.put('/instructors', instructors.put)
+routes.delete('/instructors', instructors.delete)  
 
-routes.get('/members', function(req, res) {
-    return res.send('members')
-})
+routes.get('/members', function(req, res) { return res.send('members') })
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const methodOverride = require('method-override')
 const routes = require('./routes/routes')
 const server = express()
 
 server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
+server.use(methodOverride('_method'))
 server.use(routes)
 server.set("view engine", "njk")
 
